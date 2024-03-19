8ef9ea654ec59b293388f846a92973a7c3d27288
Evgeniy <yaremenko.x@gmail.com>
2024-03-19 23:26:27 +0200

    add ParagraphComponent

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,18 +1,20 @@
-    import React from "react";
+import React from "react";
 
-    export default function Button({ onClick, getData, addSingleJoke, addMultipleJokes,}) {
-
-        const handleClick = async (event) => {
-            event.preventDefault()
-            onClick()
-            const jokeData = await getData()
-            if (jokeData && Array.isArray(jokeData)) {
-                addMultipleJokes(jokeData);
-            } else {
-                addSingleJoke(jokeData);
-            }
-        }
-        return (
-            <button onClick={handleClick}>Get a Joke</button>
-        )
+export default function Button({
+  onClick,
+  getData,
+  addSingleJoke,
+  addMultipleJokes,
+}) {
+  const handleClick = async (event) => {
+    event.preventDefault();
+    onClick();
+    const jokeData = await getData();
+    if (jokeData && Array.isArray(jokeData)) {
+      addMultipleJokes(jokeData);
+    } else {
+      addSingleJoke(jokeData);
     }
+  };
+  return <button onClick={handleClick}>Get a Joke</button>;
+}
